f1114633bb0971b838aa8fdbb1d8c6d2da60fc15
G Sai Teja Manikyala Rao <114496174+Saiteja18112004@users.noreply.github.com>
2025-03-06 22:34:17 +0530

    Add files via upload

---
+++ PYTHON CODES/PACKAGES/main.py
@@ -0,0 +1,8 @@
+from PACKAGES import ClassOne, ClassTwo
+
+if __name__ == "__main__":
+    obj1 = ClassOne()  # ✅ Calls constructor of ClassOne
+    obj1.method_one()  # ✅ Calls method of ClassOne
+
+    obj2 = ClassTwo()  # ✅ Calls constructor of ClassTwo
+    obj2.method_two()  # ✅ Calls method of ClassTwo
